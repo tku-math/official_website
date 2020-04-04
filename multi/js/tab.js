b017cac2b57897a2f2709838605f9d8582ec8239
LiYu <magikarp0831@gmail.com>
2020-04-04 18:34:57 +0800

    none

--- multi/js/tab.js
+++ multi/js/tab.js
@@ -55,235 +55,42 @@ function init() {
 }
 
 window.onload = init();
+// indes_news
+function previous(){
+	
+	new_page = parseInt($('#current_page').val()) - 1;
+	//if there is an item before the current active link run the function
+	if($('.active_page').prev('.page_link').length==true){
+		go_to_page(new_page);
+	}
+	
+}
 
-jQuery(function ($) {
-	'use strict';
-
-	// Navigation Scroll
-	$(window).scroll(function (event) {
-		Scroll();
-	});
-
-	$('.navbar-collapse ul li a').on('click', function () {
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top - 5
-		}, 1000);
-		return false;
-	});
-
-	// User define function
-	function Scroll() {
-		var contentTop = [];
-		var contentBottom = [];
-		var winTop = $(window).scrollTop();
-		var rangeTop = 200;
-		var rangeBottom = 500;
-		$('.navbar-collapse').find('.scroll a').each(function () {
-			contentTop.push($($(this).attr('href')).offset().top);
-			contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
-		})
-		$.each(contentTop, function (i) {
-			if (winTop > contentTop[i] - rangeTop) {
-				$('.navbar-collapse li.scroll')
-					.removeClass('active')
-					.eq(i).addClass('active');
-			}
-		})
-	};
-
-	$('#tohash').on('click', function () {
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top - 5
-		}, 1000);
-		return false;
-	});
-
-	// accordian
-	$('.accordion-toggle').on('click', function () {
-		$(this).closest('.panel-group').children().each(function () {
-			$(this).find('>.panel-heading').removeClass('active');
-		});
-
-		$(this).closest('.panel-heading').toggleClass('active');
-	});
-
-	//Slider
-	$(document).ready(function () {
-		var time = 7; // time in seconds
-
-		var $progressBar,
-			$bar,
-			$elem,
-			isPause,
-			tick,
-			percentTime;
-
-		//Init the carousel
-		$("#main-slider").find('.owl-carousel').owlCarousel({
-			slideSpeed: 500,
-			paginationSpeed: 500,
-			singleItem: true,
-			navigation: true,
-			navigationText: [
-				"<i class='fa fa-angle-left'></i>",
-				"<i class='fa fa-angle-right'></i>"
-			],
-			afterInit: progressBar,
-			afterMove: moved,
-			startDragging: pauseOnDragging,
-			//autoHeight : true,
-			transitionStyle: "fadeUp"
-		});
-
-		//Init progressBar where elem is $("#owl-demo")
-		function progressBar(elem) {
-			$elem = elem;
-			//build progress bar elements
-			buildProgressBar();
-			//start counting
-			start();
-		}
-
-		//create div#progressBar and div#bar then append to $(".owl-carousel")
-		function buildProgressBar() {
-			$progressBar = $("<div>", {
-				id: "progressBar"
-			});
-			$bar = $("<div>", {
-				id: "bar"
-			});
-			$progressBar.append($bar).appendTo($elem);
-		}
-
-		function start() {
-			//reset timer
-			percentTime = 0;
-			isPause = false;
-			//run interval every 0.01 second
-			tick = setInterval(interval, 10);
-		};
-
-		function interval() {
-			if (isPause === false) {
-				percentTime += 1 / time;
-				$bar.css({
-					width: percentTime + "%"
-				});
-				//if percentTime is equal or greater than 100
-				if (percentTime >= 100) {
-					//slide to next item 
-					$elem.trigger('owl.next')
-				}
-			}
-		}
-
-		//pause while dragging 
-		function pauseOnDragging() {
-			isPause = true;
-		}
-
-		//moved callback
-		function moved() {
-			//clear interval
-			clearTimeout(tick);
-			//start again
-			start();
-		}
-	});
-
-	//Initiat WOW JS
-	new WOW().init();
-	//smoothScroll
-	smoothScroll.init();
-
-	// calculus filter
-	$(window).load(function () {
-		'use strict';
-		var $calculus_selectors = $('.calculus-filter >li>a');
-		var $calculus = $('.calculus-items');
-		$calculus.isotope({
-			itemSelector: '.calculus-item',
-			layoutMode: 'fitRows'
-		});
-
-		$calculus_selectors.on('click', function () {
-			$calculus_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$calculus.isotope({
-				filter: selector
-			});
-			return false;
-		});
-	});
-
-	$(document).ready(function () {
-		//Animated Progress
-		$('.progress-bar').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-			if (visible) {
-				$(this).css('width', $(this).data('width') + '%');
-				$(this).unbind('inview');
-			}
-		});
-
-		//Animated Number
-		$.fn.animateNumbers = function (stop, commas, duration, ease) {
-			return this.each(function () {
-				var $this = $(this);
-				var start = parseInt($this.text().replace(/,/g, ""));
-				commas = (commas === undefined) ? true : commas;
-				$({
-					value: start
-				}).animate({
-					value: stop
-				}, {
-					duration: duration == undefined ? 1000 : duration,
-					easing: ease == undefined ? "swing" : ease,
-					step: function () {
-						$this.text(Math.floor(this.value));
-						if (commas) {
-							$this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-						}
-					},
-					complete: function () {
-						if (parseInt($this.text()) !== stop) {
-							$this.text(stop);
-							if (commas) {
-								$this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-							}
-						}
-					}
-				});
-			});
-		};
-
-		$('.animated-number').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-			var $this = $(this);
-			if (visible) {
-				$this.animateNumbers($this.data('digit'), false, $this.data('duration'));
-				$this.unbind('inview');
-			}
-		});
-	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function (event) {
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function () {
-				form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
-			}
-		}).done(function (data) {
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		});
-	});
-
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-
-});
+function next(){
+	new_page = parseInt($('#current_page').val()) + 1;
+	//if there is an item after the current active link run the function
+	if($('.active_page').next('.page_link').length==true){
+		go_to_page(new_page);
+	}
+	
+}
+function go_to_page(page_num){
+	//get the number of items shown per page
+	var show_per_page = parseInt($('#show_per_page').val());
+	
+	//get the element number where to start the slice from
+	start_from = page_num * show_per_page;
+	
+	//get the element number where to end the slice
+	end_on = start_from + show_per_page;
+	
+	//hide all children elements of pagingBox div, get specific items and show them
+	$('#pagingBox').children().css('display', 'none').slice(start_from, end_on).css('display', 'block');
+	
+	/*get the page link that has longdesc attribute of the current page and add active_page class to it
+	and remove that class from previously active page link*/
+	$('.page_link[longdesc=' + page_num +']').addClass('active_page').siblings('.active_page').removeClass('active_page');
+	
+	//update the current page input field
+	$('#current_page').val(page_num);
+}
