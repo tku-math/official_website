fd7359a303358be1aebbbd7b2ca014f196136f9f
LiYu <magikarp0831@gmail.com>
2020-04-09 02:26:39 +0800

    Human Computation

--- multi/js/json.js
+++ multi/js/json.js
@@ -5,16 +5,17 @@ jQuery(function ($) {
     //member.html
     function memberPhotoItem(item) {
         return `
-		<div class="calculus-item ${item.job} col-md-3 col-xs-6 col-sm-4">
+        <label for="${item.name_en}" class="calculus-item ${item.job} col-md-3 col-xs-6 col-sm-4">
             <div class="calculus-item-inner" >
-				<img class="img-responsive" src="images/members/${item.job}/${item.name_zh}.jpg">
+                <img class="img-responsive" src="images/members/${item.job}/${item.name_zh}.jpg">
                 <div class="calculus-info">
                     <h3>${item.name_zh}</h3>${item.job_title}
-					<a class="preview fa fa-eye" href="#${item.name_en}"></a>
+                    <a class="preview fa fa-eye"></a>
                 </div>
             </div>
-        </div>`
+        </label>`
     }
+
     function memberInfoItem(item) {
         function experienceInfoItem(item) {
             var count_key = Object.keys(item.experience).length
@@ -36,16 +37,6 @@ jQuery(function ($) {
             return courseArray
         }
 
-        function researchInfoItem(item) {
-            var count_key = Object.keys(item.research).length
-            var count_value = Object.values(item.research)
-            var researchArray = []
-            for (var i = 0; i < count_key; i++) {
-                researchArray += '<li>' + count_value[i] + '</li>'
-            }
-            return researchArray
-        }
-
         function contactInfoItem(item) {
             var count_key = Object.keys(item.contact).length
             var count_value = Object.values(item.contact)
@@ -54,6 +45,16 @@ jQuery(function ($) {
                 contactArray += '<li>' + count_value[i] + '</li>'
             }
             return contactArray
+        }
+    
+        function researchInfoItem(item) {
+            var count_key = Object.keys(item.research).length
+            var count_value = Object.values(item.research)
+            var researchArray = []
+            for (var i = 0; i < count_key; i++) {
+                researchArray += '<li>' + count_value[i] + '</li>'
+            }
+            return researchArray
         }
 
         function teachInfoItem(item) {
@@ -66,37 +67,42 @@ jQuery(function ($) {
             return teachArray
         }
         return `
-		<div class="lightbox" id="${item.name_en}">
-			<figure>
-				<a href="#" class="close"></a>
-				<figcaption>
-				<h1>${item.name_zh} ${item.name_en}</h1>
-                    <div class="container">
-                        <div class="row">
-                            <div class="calculus_info col-sm-6 col-xs-4 col-md-4">
-                                <h2>${item.job_title}</h2>
-                                <h3 class="fa fa-list"> 經歷</h3><br>${experienceInfoItem(item)}
-                                <h3 class="fa fa-book"> 講授課程</h3><br>${courseInfoItem(item)}
-                            </div>
-                            <div class="calculus_info col-sm-6 col-xs-4 col-md-4">
-                                <img class="img-responsive" src="images/members/${item.job}/${item.name_zh}.jpg">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="calculus_info col-sm-6 col-xs-4 col-md-4">
-                                <h3 class="fa fa-book"> 研究領域</h3><br>${researchInfoItem(item)}
-                            </div>
-                            <div class="calculus_info col-sm-3 col-xs-5 col-md-2">
-                                <h3 class="fa fa-book"> 聯絡資訊</h3><br>${contactInfoItem(item)}
-                            </div>
-                            <div class="calculus_info col-sm-3 col-xs-5 col-md-2">
-                                <h3 class="fa fa-book"> 教學資源</h3><br>${teachInfoItem(item)}
-                            </div>
+        <div class="popup__container">
+            <input type="checkbox" id="${item.name_en}" class="popup__toggler" />
+            <label class="popup__mask" for="${item.name_en}"></label>
+            <div class="popup">
+                <label class="popup__close" for="${item.name_en}"></label>
+                <div class="popup__header">
+                    <h1 class="popup__title">${item.name_zh} ${item.name_en}</h1>
+                </div>
+                <div class="popup__content">
+                    <h2>${item.job_title}</h2>
+                    <div class="row calculus_info">
+                        <img class="img-responsive col-md-6" src="images/members/${item.job}/${item.name_zh}.jpg">
+                        <div class="col-md-6">
+                            <h3 class="fa fa-list">經歷</h3>${experienceInfoItem(item)}
+                            <h3 class="fa fa-book"> 講授課程</h3>${courseInfoItem(item)}
                         </div>
                     </div>
-				</figcaption>
-			</figure>
-		</div>`
+                    <div class="row calculus_info">
+                        <div class="col-md-4">
+                            <h3 class="fa fa-book"> 聯絡資訊</h3>${contactInfoItem(item)}
+                        </div>
+                        <div class="col-md-4">
+                            <h3 class="fa fa-book"> 研究領域</h3><br>${researchInfoItem(item)}
+                        </div>
+                        <div class="col-md-4">
+                            <h3 class="fa fa-book"> 教學資源</h3>${teachInfoItem(item)}
+                        </div>
+                    </div>
+                    <div>
+                        <h3>科技部研究案</h3>
+                        <h3>期刊論文</h3>
+                        <h3>會議論文</h3>
+                    </div>
+                </div>
+            </div>
+        </div>`
     }
     fetch('/json/member.json')
         .then(function (response) {
